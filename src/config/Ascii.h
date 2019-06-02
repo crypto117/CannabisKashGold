38d8e142a2ef5f049134fdc56379491d228c6e97
Brother Kai <46241441+crypto117@users.noreply.github.com>
2019-06-01 21:17:20 -0700

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -4,28 +4,20 @@
 
 #pragma once
 
-const std::string windowsAsciiArt =
-      "\n _______         _   _       _____      _        \n"
-              "|__   __|       | | | |     / ____|    (_)      \n"
-              "   | |_   _ _ __| |_| | ___| |     ___  _ _ __  \n"
-              "   | | | | | '__| __| |/ _ \\ |    / _ \\| | '_ \\ \n"
-              "   | | |_| | |  | |_| |  __/ |___| (_) | | | | |\n"
-      "   |_|\\__ _|_|   \\__|_|\\___|\\_____\\___/|_|_| |_|\n";
-
 const std::string nonWindowsAsciiArt = 
       "\n                                                                            \n"
-        "████████╗██╗  ██╗██████╗ ████████╗██╗    ██████╗ █████╗ █████╗ ██╗███╗   ██╗\n"
-        "╚══██╔══╝██║  ██║██╔══██╗╚══██╔══╝██║    ██╔═══╝██╔═══╝██╔══██╗██║████╗  ██║\n"
-        "   ██║   ██║  ██║██████╔╝   ██║   ██║    ████╗  ██║    ██║  ██║██║██╔██╗ ██║\n"
-        "   ██║   ██║  ██║██╔══██╗   ██║   ██║    ██╔═╝  ██║    ██║  ██║██║██║╚██╗██║\n"
-        "   ██║   ╚█████╔╝██║  ██║   ██║   ██████╗██████╗╚█████╗╚█████╔╝██║██║ ╚████║\n"
-        "   ╚═╝    ╚════╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝╚═════╝ ╚════╝ ╚════╝ ╚═╝╚═╝  ╚═══╝\n";
+        "xxxxxxxxxxxxxxxxxxxxxxx000000X0000xxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxxxxxx0000000000XX000000000xxxxxxxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxxxXX00000000000XXX000000000XXxxxxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxx00XXX000000000XXXXX0000000XX000xxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxx0000XXXX0000000XXXXX000000XXX00000xxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxx0000000XXXX0000XXXXXXX000XXXX0000000xxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxx00000000XXXXX000XXXXX000XXXXX0000000xxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxx000XXXXXXXXXXXXXXXXXXXXXXXXXXXX000xxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxx0000000XXXXXXXXXXXXXXXXXX0000000xxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxx0000XXXXXXXXXXXXXXXXXXXXXX0000xxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxxxxXX00000XXXX0X0XXXX000000XXxxxxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxxxxx0000XXX0000X0000XXX0000xxxxxxxxxxxxxxxxxxxxxxx\n"
+        "xxxxxxxxxxxxxxxxxxxxXX000000Xx000000XXxxxxxxxxxxxxxxxxxxxxxxxxx\n";
 
-/* Windows has some characters it won't display in a terminal. If your ascii
-   art works fine on Windows and Linux terminals, just replace 'asciiArt' with
-   the art itself, and remove these two #ifdefs and above ascii arts */
-#ifdef _WIN32
-const std::string asciiArt = windowsAsciiArt;
-#else
-const std::string asciiArt = nonWindowsAsciiArt;
-#endif
